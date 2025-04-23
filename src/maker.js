4473f4f61ac6551f3cc4c7a26a5a63925a431cb9
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-04-22 19:00:03 -0500

    add a hide and unhide button

--- src/maker.js
+++ src/maker.js
@@ -47,6 +47,16 @@ function updateBuilt() {
     document.getElementById("built-list-out").innerHTML = list;
 }
 
+function hideBuildItems() {
+    document.getElementById('built-list').style = 'display: none;'
+    document.getElementById('hide').innerHTML = '<svg class="icon-add" onclick="unhideBuildItems()" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>';
+}
+
+function unhideBuildItems() {
+    document.getElementById('built-list').style = 'display: block';
+    document.getElementById('hide').innerHTML = '<svg class="icon-add" onclick="hideBuildItems()" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z"/></svg>'
+}
+
 function textUpdate(input, output) {
     document.getElementById(output).innerHTML = document.getElementById(input).value;
 }
